bb13f117903b3d19ba4d9221edb14d5da4287065
v_dh <vincent.dhalluin@gmail.com>
2018-10-24 14:29:46 +0200

    Initial Commit

---
+++ DevrapLog/DevrapLog.h
@@ -0,0 +1,19 @@
+//
+//  DevrapLog.h
+//  DevrapLog
+//
+//  Created by vdh on 24/10/2018.
+//  Copyright © 2018 devr4p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DevrapLog.
+FOUNDATION_EXPORT double DevrapLogVersionNumber;
+
+//! Project version string for DevrapLog.
+FOUNDATION_EXPORT const unsigned char DevrapLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevrapLog/PublicHeader.h>
+
+
